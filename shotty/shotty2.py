8f8d7cd1dc823ad3377beb0f9fc96e1bb9a82b16
Hrithik Singh <hrithik.singh1509@gmail.com>
2020-07-01 01:04:10 +0530

    Updated more info about instances

--- shotty/shotty2.py
+++ shotty/shotty2.py
@@ -1,6 +1,13 @@
 import boto3
-if __name__=='__main__':
-    session2=boto3.Session(profile_name='shotty2')
-    ec2=session2.resource('ec2')
+#import click
+session2=boto3.Session(profile_name='shotty2')
+ec2=session2.resource('ec2')
+#@click.command()
+def list_instances():
     for i in ec2.instances.all():
-        print(i)
+        print(i.id,' ',i.instance_type,' ',i.state['Name'],' ',i.public_dns_name,' ',i.placement['AvailabilityZone'])
+
+    return
+
+if __name__=='__main__':
+    list_instances()
